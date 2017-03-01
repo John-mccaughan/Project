b74adac0c50a02cbe05e7d251486f4631d0ab0fd
John-mccaughan <john_mccaughan@hotmail.com>
2017-03-01 13:51:39 +1300

    1.2
    Changes to tile selection

--- Project Drogoth 1.1/Assets/Script/MouseMan.cs
+++ Project Drogoth 1.1/Assets/Script/MouseMan.cs
@@ -3,36 +3,23 @@ using System.Collections;
 
 public class MouseMan : MonoBehaviour {
 
-
+    private Color basicColor = Color.green;
+    private Color hoverColor = Color.red;
+    private Renderer renderer;
 
     void Start()
     {
-		
-    }
-    //Update is called once per frame
-    void Update () {
-
-        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		Debug.DrawRay (ray.origin,ray.direction);
-        RaycastHit hitInfo;
-        if (Physics.Raycast(ray, out hitInfo)){
-            GameObject hitObj = hitInfo.collider.transform.gameObject;
-            if(hitObj.tag == "Ground")
-           {
-                MeshRenderer mr = hitObj.GetComponentInChildren<MeshRenderer>();
-                mr.material.color = Color.red;
-            }
-        }
-
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = Color.black;
     }
 
-    //void OnMouseEnter()
-    //{
-     //   renderer.material.color = Color.red;
-    //}
+    void OnMouseEnter()
+    {
+        renderer.material.color = Color.red;
+    }
 
-    //void OnMouseExit()
-    //{
-       // renderer.material.color = Color.black;
-   // }
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.black;
+    }
 }
