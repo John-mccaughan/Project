87afb73616427b9b6db27948b9357043902a7793
John-mccaughan <john_mccaughan@hotmail.com>
2017-03-07 19:12:46 +1300

    1.2.4
    Trees and Textures add to tiles

--- Project Drogoth 1.1/Assets/Script/TileMapGen.cs
+++ Project Drogoth 1.1/Assets/Script/TileMapGen.cs
@@ -8,8 +8,8 @@ public class TileMapGen : MonoBehaviour {
 	public DrawMode drawMode;
 
 	const int mapChunkSize = 100;
-	[Range(0,6)]
-	public int levelOfDetail;
+	//[Range(0,6)]
+	//public int levelOfDetail;
 	public float noiseScale;
 
 	public int octaves;
@@ -27,9 +27,12 @@ public class TileMapGen : MonoBehaviour {
 
 	//public TarrainType[] regions;
 
-	public GameObject hexObj1;
-	public GameObject hexObj2;
-	public GameObject hexObj3;
+	public GameObject GrassHexObj1;
+	public GameObject WaterHexObj1;
+	public GameObject WaterHexObj2;
+	public GameObject SandHexObj;
+	public GameObject treeHexObj1;
+	public GameObject treeHexObj2;
 
 	float offRowXOffSet = 1.519f;
 	float offRowZOffSet = 1.315f;
@@ -38,6 +41,7 @@ public class TileMapGen : MonoBehaviour {
 
 	public void GenerateMap()
 	{
+		int num;
 		tiles = new GameObject();
 		tiles.transform.SetParent(this.transform);
 		float[,] noiseMap = Noise.GenNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
@@ -46,29 +50,50 @@ public class TileMapGen : MonoBehaviour {
 		for (int y = 0; y < mapChunkSize; y++) {
 			for (int x = 0; x < mapChunkSize; x++) {
 				float currentHeight = TileHeightCurve.Evaluate(noiseMap[x,y]) * TileHeightMultiplier;
-				print (currentHeight);
+				//print (currentHeight);
 				float xPos = x * offRowXOffSet;
 				if(y % 2 == 1)
 				{
 					xPos += offRowXOffSet/2f;
 				}
-				if (noiseMap[x,y] < .2f) {
-					GameObject hex_goWater = (GameObject)Instantiate (hexObj2, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
+				if (noiseMap [x, y] < .2f) {
+					GameObject hex_goWater = (GameObject)Instantiate (WaterHexObj1, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
 					hex_goWater.name = "WaterHex_" + x + "_" + y;
 					hex_goWater.transform.SetParent (tiles.transform);
 					hex_goWater.isStatic = true;
-				}
-				else if (noiseMap[x,y] < .39f && noiseMap[x,y] > .15f) {
-					GameObject hex_goWater1 = (GameObject)Instantiate (hexObj3, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
+				} else if (noiseMap [x, y] < .39f && noiseMap [x, y] > .15f) {
+					GameObject hex_goWater1 = (GameObject)Instantiate (WaterHexObj2, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
 					hex_goWater1.name = "WaterHex_" + x + "_" + y;
 					hex_goWater1.transform.SetParent (tiles.transform);
 					hex_goWater1.isStatic = true;
+				}
+					else if (noiseMap[x,y] < .45f && noiseMap[x,y] > .39f) {
+						GameObject hex_goSand1 = (GameObject)Instantiate (SandHexObj, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
+						hex_goSand1.name = "SandHex_" + x + "_" + y;
+						hex_goSand1.transform.SetParent (tiles.transform);
+						hex_goSand1.isStatic = true;
 				} else {
-
-					GameObject hex_go1 = (GameObject)Instantiate (hexObj1, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
+					num = Random.Range (1, 50);
+					print ("Num" + num);
+					if(num >= 3 ){
+					GameObject hex_go1 = (GameObject)Instantiate (GrassHexObj1, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
 					hex_go1.name = "Hex1_" + x + "_" + y;
 					hex_go1.transform.SetParent (tiles.transform);
 					hex_go1.isStatic = true;
+					}
+					else if(num == 1){ 
+						GameObject hex_go2 = (GameObject)Instantiate (treeHexObj1, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
+						hex_go2.name = "treeHex1_" + x + "_" + y;
+						hex_go2.transform.SetParent (tiles.transform);
+						hex_go2.isStatic = true;
+					}
+					else if(num == 2){ 
+						GameObject hex_go3 = (GameObject)Instantiate (treeHexObj2, new Vector3 (xPos, currentHeight, y * offRowZOffSet), Quaternion.identity);
+						hex_go3.name = "treeHex2_" + x + "_" + y;
+						hex_go3.transform.SetParent (tiles.transform);
+						hex_go3.isStatic = true;
+					}
+
 				}
 			}
 		}
